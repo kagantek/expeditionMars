995c777089b128265aa0af62a4ec7f83c4afa5b3
Kagan Tek <kagantek2003@hotmail.com>
2024-03-17 16:23:18 +0300

    Improved Camera Controller

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 playerPosition;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,18 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        if (player.transform.localScale.x > 0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+
     }
 }
